6301300170ee9cc8914d46c0dc2f66984b9606e3
Артём Тюрин <zientenin@mail.ru>
2024-04-17 05:58:00 +0300

    Add method for get info about tours

--- core/app.py
+++ core/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 
 from constants import FLASK_PORT
 import http_codes
+import db_queries
 from utils import get_connection
 
 app = Flask(__name__)
@@ -12,6 +13,13 @@ connection = get_connection()
 @app.get('/')
 def index_page() -> tuple[str, int]:
     return '<p>Welcome on tour crud index page</p>', http_codes.OK   
+
+
+@app.get('/tours')
+def get_tours() -> tuple[str, int]:
+    with connection.cursor() as cursor:
+        cursor.execute(db_queries.GET_TOURS)
+    return cursor.fetchall(), http_codes.OK
 
 if __name__ == '__main__':
     app.run(port=FLASK_PORT)
