611f5826553023a763e794e7dc79b7f431076c68
Артём Тюрин <zientenin@mail.ru>
2024-04-19 02:30:31 +0300

    Add docker compose

--- core/constants.py
+++ core/constants.py
@@ -3,7 +3,7 @@ from os import getenv
 
 load_dotenv()
 
-pg_fields = ['PG_HOST', 'PG_PORT', 'PG_USER', 'PG_PASSWORD', 'PG_DBNAME']
+pg_fields = ['POSTGRES_HOST', 'POSTGRES_PORT', 'POSTGRES_USER', 'POSTGRES_PASSWORD', 'POSTGRES_DB']
 
 HOST, PORT, USER, PASSWORD, DBNAME = [getenv(param) for param in pg_fields]
 PORT = int(PORT) if PORT and PORT.isdigit() else None
