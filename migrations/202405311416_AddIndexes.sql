977ce4efca6e0abb877dd1c97e1fd4e60f748db8
Артём Тюрин <zientenin@mail.ru>
2024-05-31 19:30:45 +0300

    Add indexes

--- migrations/202405311416_AddIndexes.sql
+++ migrations/202405311416_AddIndexes.sql
@@ -1,5 +1,5 @@
 -- migrate:up
 create extension if not exists pg_trgm;
 create index tour_name_trgm_idx on tour_data.tour using gist(name gist_trgm_ops);
-create index city_name_idx on tour_data.city using btree(name);
+CREATE INDEX idx_agency_rating ON tour_data.agency USING btree(rating);
 -- migrate:down
